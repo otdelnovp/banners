d4534ea18aaca62a9c96e25bc322cccd9cbf0043
Pavel Otdelnov <p4sh0k@gmail.com>
2018-03-31 12:54:44 +0300

    up

---
+++ megafon/aksel/full/core.js
@@ -0,0 +1,18 @@
+var gn;
+
+function init_gn() {
+
+  gn = new GyroNorm();
+
+  gn.init(args).then(function() {
+    var isAvailable = gn.isAvailable();
+    gn.start(gnCallBack);
+  }).catch(function(e){
+    console.log(e);
+  });
+	
+}
+
+function gnCallBack(data) {
+  $('#do_gamma').val(data.do.gamma);
+}
