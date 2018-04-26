656a4c3e0d713ce6f38299090eadabb845781990
Pavel Otdelnov <p4sh0k@gmail.com>
2018-04-26 16:55:14 +0300

    up

---
+++ curious/312х300/scripts/script.js
@@ -0,0 +1,8 @@
+function initAD() {
+    document.getElementById("fond").style.opacity = 1;
+    document.getElementById("cta").style.opacity = 1;
+    document.getElementById("logo").style.opacity = 1;
+    document.getElementById("claim").style.opacity = 1;
+}
+
+window.addEventListener("load", initAD);
