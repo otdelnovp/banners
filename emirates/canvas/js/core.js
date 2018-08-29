b6a176e70d1f8678c7b3768fe2fafb9ab2f8d97c
Pavel Otdelnov <p4sh0k@gmail.com>
2018-08-29 15:18:23 +0300

    up

--- emirates/canvas/js/core.js
+++ emirates/canvas/js/core.js
@@ -1,39 +1,50 @@
-$(document).ready(function() {
-
-	// slider
-	$('.slider').owlCarousel({
-		loop: false,
-		rewind: true,
-		margin: 0,
-		nav: true,
-		navText: [ '', '' ],
-		dots: false,
-		animateOut: 'fadeOut',
-		items: 1
-	});
-
-	// space
-	// document.onkeydown = function checkKeycode(e) {
-	// 	if (e.which == 32) {
-	// 		$('.owl-item.active .item').addClass('revert');
-	// 	}
-	// };
-	document.onkeyup = function checkKeycode(e) {
-		if (e.which == 32) {
-			$('.owl-item.active .item').toggleClass('revert');
+// video
+var gebi = function(id) { return document.getElementById(id) };
+window.onload = function() {
+	var vid = gebi('vid');
+	var vidPlay = gebi('vid_play');
+	var vidMute = gebi('vid_mute');
+	var onClickPlay = function(e) {
+		e.preventDefault();
+		if (vid.paused) {
+			vid.play();
+			vidPlay.classList.remove('video__play');
+			vidPlay.classList.add('video__pause');
+		} else {
+			vid.pause();
+			vidPlay.classList.add('video__play');
+			vidPlay.classList.remove('video__pause');
 		}
 	};
+	vidPlay.addEventListener('click', onClickPlay);
+	vid.addEventListener('click', onClickPlay);
+	vidMute.addEventListener('click', function(e) {
+		e.preventDefault();
+		if (vid.muted) {
+			vid.muted = false;
+			vidMute.classList.remove('video__mute');
+			vidMute.classList.add('video__mute-off');
+		} else {
+			vid.muted = true;
+			vidMute.classList.add('video__mute');
+			vidMute.classList.remove('video__mute-off');
+		}
+	});
 
-});
+	vid.play();
+	vidPlay.classList.remove('video__play');
+	vidPlay.classList.add('video__pause');
+};
+
 
 // auto size
 function remCalibrate() {
 	var maxRem = 13,
-		minRem = 5;
+		minRem = 3;
 	var w = window.innerWidth,
 		h = window.innerHeight;
-	var sizeWidth = w / 96,
-		sizeHeight = h / 68;
+	var sizeWidth = w / 150,
+		sizeHeight = h / 108;
 	var remSize = (sizeWidth < sizeHeight) ? sizeWidth : sizeHeight;
 	remSize = (remSize > maxRem) ? maxRem :
 		(remSize < minRem) ? minRem : remSize;
