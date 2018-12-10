17a1ea1dff99c99c9f538824c4cac01898ffaae1
Pavel Otdelnov <p4sh0k@gmail.com>
2018-12-10 19:59:43 +0300

    up

--- curious/312х300/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function initAD() {
-    document.getElementById("fond").style.opacity = 1;
-    document.getElementById("cta").style.opacity = 1;
-    document.getElementById("logo").style.opacity = 1;
-    document.getElementById("claim").style.opacity = 1;
-}
-
-window.addEventListener("load", initAD);
